accb6f25a593792b68b300aa82ce50ea33b72210
RafaelMSuarez <rafael.martinez1601@alumnos.ubiobio.cl>
2024-04-16 12:18:18 -0400

    profile page

--- lib/pages/profile_page/profile_page.dart
+++ lib/pages/profile_page/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_ubb/style/padding_style.dart';
+import 'package:proyecto_ubb/style/text_styles.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,8 +12,57 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Página de perfil y configuración"),
+    double alto = MediaQuery.of(context).size.height;
+    double ancho = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Padding(
+        padding: PaddingTheme.horizontal,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: alto * 0.05, bottom: 15),
+              child: const Text(
+                "Cambiar nombre",
+                style: TitleTheme.secondTitle,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  // labelText: "Nombre",
+                  prefixIcon: Icon(Icons.person),
+                  contentPadding: EdgeInsets.zero,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: Container(
+                color: Colors.grey[300],
+                height: alto * 0.2,
+                width: ancho,
+                child: const Text("links información"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: Container(
+                color: Colors.grey[300],
+                height: alto * 0.35,
+                width: ancho,
+                child: const Text("informacion app y creditos"),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
